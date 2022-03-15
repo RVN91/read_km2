0d620874e59bd729469a3b48a04fa70cdefacb9f
rvn <rn@civil.dk>
2022-03-15 13:32:13 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# readme_km2
+# Read KMD2-formatted files from the Danish SVK-raingauges
+
+## Description
+
+Reads the KMD2 input file and saves it to a CSV-file. Function can be included
+into existing scripts or used separately.
+
+### Input
+
+* file_name:   Name of the input file.
+* save_to_csv: Boolean, if "True", a csv file will be written to disk.
+
+### User guide
+Type the following command into the terminal:
+
+python3 water_level.py <station_number> <start_date> <end_date>
+Date format: dd-mm-yyyy
+
+Example:        
+python3 water_level.py 20253 01-01-2018 01-01-2019
--- read_km2.py
+++ read_km2.py
@@ -1,73 +1,90 @@
+# encoding: utf8   
+## Description
+# Name: read_km2.py
+#
+# Author: Rasmus Nielsen, 2013
+#
+
+## Updates:
+# * (15-03-2022) Fixed no_of_lines to be a integer
+# * (15-03-2022) File handle is now opening as "r" instead of "rb"
+
+# Author: Rasmus Nielsen, 2013
+
+
 import re, pandas as pd
 from datetime import timedelta, datetime
 
 def read_kmd2(file_name, save_to_csv):
-	"""
-	Reads a kmd2 file and returns the time series. If save_to_csv is true
-	then the time series is stored on the disk.
+    """
+    Reads a kmd2 file and returns the time series. If save_to_csv is true
+    then the time series is stored on the disk.
 
-	return: dictionary with rain_series[start_date] = (minutes of rain,
-													   time stamps, data)
-	"""
+    return: dictionary with rain_series[start_date] = (minutes of rain,
+                                                       time stamps, data)
+    """
 
-	# Load kmd2 file
-	with open(file_name, 'rb') as file:
-		lines = file.readlines()
+    # Load kmd2 file
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
 
-	# List of data frames with the time series
-	df_list = []
+    # List of data frames with the time series
+    df_list = []
 
-	# Go through each line and read the appropiate lines of data
-	for i, line in enumerate(lines):
-		# Check if header is detected
-		if re.findall('\d\s\d+\s+\d+\s+\d+\s+\s+\s+\d+\s+\d+\s+\d+\.\d+\s+\d+', line):
-			# Set temporary result dictionary
-			rain_series = {}
+    # Go through each line and read the appropiate lines of data
+    for i, line in enumerate(lines):
+        # Check if header is detected
+        if re.findall('\d\s\d+\s+\d+\s+\d+\s+\s+\s+\d+\s+\d+\s+\d+\.\d+\s+\d+', line):
+            # Set temporary result dictionary
+            rain_series = {}
 
-			# Extract the number of minutes of data
-			match = re.search('\d\s\d+\s+\d+\s+\d+\s+(\d+)', line)
-			no_of_data = int(match.group(1))
-			
-			# Extract the start data
-			match = re.search('\d\s(\d+\s+\d+)', line)
-			start_date = datetime.strptime(match.group(1), '%Y%m%d %H%M')
-			
-			# Store the lines of data
-			# Determine how many lines the data spans
-			# Each line contains 10 data points in a minutely resolution
-			if no_of_data <= 10: # Only 1 line long
-				no_of_lines = 1
-			# If the span is greater than 10 minutes
-			# subtract the remainder from the number 
-			# of lines
-			else: 
-				remainder = no_of_data % 10
-				if remainder > 0:
-					no_of_lines = (no_of_data - remainder) / 10 + 1
-				else:
-					no_of_lines = no_of_data / 10
-			data_raw = lines[i + 1 : i + 1 + no_of_lines]
+            # Extract the number of minutes of data
+            match = re.search('\d\s\d+\s+\d+\s+\d+\s+(\d+)', line)
+            no_of_data = int(match.group(1))
+            
+            # Extract the start data
+            match = re.search('\d\s(\d+\s+\d+)', line)
+            start_date = datetime.strptime(match.group(1), '%Y%m%d %H%M')
+            
+            # Store the lines of data
+            # Determine how many lines the data spans
+            # Each line contains 10 data points in a minutely resolution
+            if no_of_data <= 10: # Only 1 line long
+                no_of_lines = 1
+            # If the span is greater than 10 minutes
+            # subtract the remainder from the number 
+            # of lines
+            else: 
+                remainder = no_of_data % 10
+                if remainder > 0:
+                    no_of_lines = (no_of_data - remainder) / 10 + 1
+                else:
+                    no_of_lines = no_of_data / 10
 
-			# Extract the data to a list
-			data = []
-			for data_line in data_raw:
-				entries = re.findall('\d+\.\d+', data_line) # Find all data entries
-				for entry in entries: 
-					data.append(float(entry))
-			
-			# Add time stamp for each data entry
-			time_stamps = [start_date + timedelta(minutes=x) for x in range(0, no_of_data)]
+            data_raw = lines[i + 1 : i + 1 + int(no_of_lines)]
 
-			# Update dictionary
-			rain_series[start_date] = (no_of_data, time_stamps, data)
+            # Extract the data to a list
+            data = []
+            for data_line in data_raw:
+                entries = re.findall('\d+\.\d+', data_line) # Find all data entries
+                for entry in entries: 
+                    data.append(float(entry))
+            
+            # Add time stamp for each data entry
+            time_stamps = [start_date + timedelta(minutes=x) for x in range(0, no_of_data)]
 
-			# Store in data frame list
-			df_list.append(pd.DataFrame(data = data, index = time_stamps, columns = 
-										[file_name.split('.')[0].split('\\')[1]]))
+            # Update dictionary
+            rain_series[start_date] = (no_of_data, time_stamps, data)
 
-	# Concatenate the data frames into one
-	df = pd.concat(df_list)
-	df.index.name = 'index'
-	# Write time series to disk
-	if save_to_csv: df.to_csv(file_name.split('.')[0] + '.csv')
-	return df
+            # Store in data frame list
+            df_list.append(pd.DataFrame(data = data, index = time_stamps, columns = 
+                                        [file_name.split('.')[0]]))
+
+    # Concatenate the data frames into one
+    df = pd.concat(df_list)
+    df.index.name = 'index'
+    # Write time series to disk
+    if save_to_csv: df.to_csv(file_name.split('.')[0] + '.csv')
+    return df
+
+read_kmd2("Example data.km2", True)
